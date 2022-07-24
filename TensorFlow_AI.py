fe07408c5778875317fbe55d86c004c1bee3c6b3
mmosaffa <52753683+mmosaffa@users.noreply.github.com>
2022-07-23 18:41:01 -0700

    Update TensorFlow_AI.py

--- TensorFlow_AI.py
+++ TensorFlow_AI.py
@@ -5,7 +5,6 @@ Created on Tue Apr  6 20:12:04 2021
 @author: Mohammad.M
 """
 
-#Here is a simple example of how to implement a deep learning to figure out the relation between X and Y
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
@@ -19,8 +18,6 @@ ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 model.fit(xs, ys, epochs=500)
 print(model.predict([10.0]))
 
-
-#This code can stop running epoches because of reaching our threshold!
 import tensorflow as tf
 
 class myCallback(tf.keras.callbacks.Callback):
@@ -47,16 +44,11 @@ model.compile(optimizer=tf.optimizers.Adam(),
 
 model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
 
-#Excercice of week 2
 import tensorflow as tf
 from os import path, getcwd, chdir
 
-# DO NOT CHANGE THE LINE BELOW. If you are developing in a local
-# environment, then grab mnist.npz from the Coursera Jupyter Notebook
-# and place it inside a local folder and edit the path to that location
 path = f"{getcwd()}/../tmp2/mnist.npz"
 
-# GRADED FUNCTION: train_mnist
 def train_mnist():
     # Please write your code only where you are indicated.
     # please do not remove # model fitting inline comments.
@@ -67,12 +59,11 @@ def train_mnist():
             if(logs.get('acc')>0.99):
               print("\nReached 99% accuracy so cancelling training!")
               self.model.stop_training = True
-    # YOUR CODE SHOULD END HERE
 
     mnist = tf.keras.datasets.mnist
 
     (x_train, y_train),(x_test, y_test) = mnist.load_data()
-    # YOUR CODE SHOULD START HERE
+
     x_train, x_test = x_train / 255.0, x_test / 255.0
     callbacks = myCallback()
     # YOUR CODE SHOULD END HERE
@@ -88,15 +79,12 @@ def train_mnist():
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
     
-    # model fitting
     history = model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
-    # model fitting
+
     return history.epoch, history.history['acc'][-1]
 
 train_mnist()
 
-
-#How Convolutions work?
 import cv2
 import numpy as np
 from scipy import misc
@@ -113,21 +101,13 @@ i_transformed = np.copy(i)
 size_x = i_transformed.shape[0]
 size_y = i_transformed.shape[1]
 
-# This filter detects edges nicely
-# It creates a convolution that only passes through sharp edges and straight
-# lines.
-
 #Experiment with different values for fun effects.
 #filter = [ [0, 1, 0], [1, -4, 1], [0, 1, 0]]
 
-# A couple more filters to try for fun!
+# A couple more filters to try!
 filter = [ [-1, -2, -1], [0, 0, 0], [1, 2, 1]]
 #filter = [ [-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
 
-# If all the digits in the filter don't add up to 0 or 1, you 
-# should probably do a weight to get it to do so
-# so, for example, if your weights are 1,1,1 1,2,1 1,1,1
-# They add up to 10, so you would set a weight of .1 if you want to normalize them
 weight  = 1
 
 for x in range(1,size_x-1):
@@ -149,7 +129,6 @@ for x in range(1,size_x-1):
         convolution=255
       i_transformed[x, y] = convolution
       
-# Plot the image. Note the size of the axes -- they are 512 by 512
 plt.gray()
 plt.grid(False)
 plt.imshow(i_transformed)
@@ -168,13 +147,8 @@ for x in range(0, size_x, 2):
     pixels.append(i_transformed[x+1, y+1])
     newImage[int(x/2),int(y/2)] = max(pixels)
 
-# Plot the image. Note the size of the axes -- now 256 pixels instead of 512
 plt.gray()
 plt.grid(False)
 plt.imshow(newImage)
 #plt.axis('off')
 plt.show()      
-
-
-    
-    
